4ce3e95c2b381b291ae3aec658d2b69d1d807ad6
johannesUIA <johannesma@student.uia.no>
2025-01-27 11:05:25 +0100

    Litt styling homepage

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
