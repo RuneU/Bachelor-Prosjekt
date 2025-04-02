c47d51f4754ab0d6461abbcc3b5945bcfc308d86
RuneU <runeundheim@live.no>
2025-04-02 11:07:22 +0200

    Slettet unødvendig routing i app.py

--- app.py
+++ app.py
@@ -124,58 +124,11 @@ def evacuee_search():
 def evacuee_update(evakuertID):
     return f"Oppdateringsside for evakuertID {evakuertID}"
 
-
-def generate_frames():
-    camera = cv2.VideoCapture(0)  
-    while True:
-        success, frame = camera.read()  
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)  
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/save_face', methods=['POST'])
-def save_face_route():
-    data = request.get_json()
-    user_id = data.get("user_id")
-
-    if not user_id:
-        return jsonify({"error": "User ID is required"}), 400
-
-    result = save_face(user_id)
-    return jsonify(result)
-
-@app.route('/static/faces/<filename>')
-def serve_face_image(filename):
-    return send_from_directory("static/faces", filename)
-
-@app.route('/faceID')
-def faceID():
-    return render_template('faceID.html')
-
-@app.route("/iot")
-def iot():
-    return render_template("iot.html")
-
-@app.route("/startID")
-def startID():
-    return render_template("startID.html")
-
 @app.route("/newuser")
 def newuser():
     return render_template("newuser.html")
 
-@app.route("/iot_login")
-def iot_login():
-    return render_template("iot_login.html")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
